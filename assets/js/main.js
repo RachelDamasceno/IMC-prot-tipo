951865bd90c7a77bc73eefe3cbb9e4d6c9c44922
Elizabeth Damasceno <rachelemilyADS@gmail.com>
2024-03-24 07:51:19 -0300

    -ADD imc e segurança para os numero

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,35 @@ const formu = document.querySelector("#formu");
 // funçao com event, o evento tem que esta entre ""
 formu.addEventListener("submit", (event) => {
   event.preventDefault(); //ele cancela o evento submit
-  console.log("evento submit cancelado");
-  setResul("Teste");
+  let inputAltura = event.target.querySelector("#altura")// captiruando todo conteudo de altura
+  let inputPeso = event.target.querySelector("#peso")// captiruando todo conteudo de peso
+  let peso = Number(inputPeso.value)// pegando o valor de peso
+  let altura = Number(inputAltura.value)// pegando o valor de altura
+
+  // Se o numero for falso retornar uma mensagem de falso para setResul e declara ele como falso
+  if(!peso){
+    
+    setResul("Valor invalido - peso", false)
+    return;// o return enterrompe tdo que esta abaixo dele
+  }
+  if(!altura){
+    setResul("Valor invalido - altura", false)
+    return;
+  }
+
+
+
+  let imc = getIMC(peso,altura)// passando o return da função get imc para a variavel imc
+
+  setResul(`O resultado é ${imc}`,true)
+
 });
+
+// função para calcular imc
+function getIMC (peso,altura){
+  let cal = peso / altura **2
+  return cal.toFixed(2); // retornando somente o resultado do imc ja com 2 casas decimais
+}
 
 // agora vamos criar uma função que ao tocar no botão enviar aparecça uma mensagem. O enviar deve ser feito na função acima(formu) ja com a mensagem, usando o setResul passamso o valor teste para o parametro msg. Nessa função de baixo vamos criar so o mecanismo de envio do texto gerado no formu para o html.
 
@@ -16,17 +42,33 @@ formu.addEventListener("submit", (event) => {
 //   resultado.innerHTML = `<p>${msg}</p>`;
 // }
 
+// funçao so para criar o paragrafo
 function setParagrafo(className) {
   const p = document.createElement("p"); //Criando um novo elemento, o que esta dentro de "" são tags html
-  p.classList.add(className); //Add um nome de classe a tag p
+ 
   return p;
 }
 
 // Outra maneira de enviar o conteudo: inserir um elemento diretamente no html. Para tanto vamos criar a variavel paragrafo que vai receber um docuent com o valor que sera trabalhado
 
-function setResul(msg) {
+function setResul(msg,isValid) {
   let resultado = document.querySelector("#resultado-html");
   resultado.innerHTML = "";
+
+  const p = setParagrafo()
+  
+  p.innerHTML = msg
+
+  if(!isValid){
+      p.classList.add('bad-html'); //Add um nome de classe a tag p caso valor de altura e peso falso
+
+  }else{
+    p.classList.add('verdadeiro-html')
+  }
+ 
+
+  resultado.appendChild(p)
+
 }
 
 // PAREI EM 24 MINUTOS
